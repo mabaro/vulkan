8157161be2ac007c0ca493b5cb527c9d3ef3d278
marcos <markisbcn@gmail.com>
2023-09-19 21:30:40 +0200

    increased c++17 and added some helpers (i.e., logging)

--- sample1/lib/gfx/include/gfx/vk_types.h
+++ sample1/lib/gfx/include/gfx/vk_types.h
@@ -13,8 +13,6 @@
         }                                                                      \
     } while (0)
 
-#define LOG_ERROR(X) fprintf(stderr, "Error: %s\n", X)
-
 /////////////////////////////////////////////////////////////////////////////////
 
 #define USING(_X_) _X_&& _X_
@@ -29,3 +27,42 @@
 #define IS_DEBUG NOT_IN_USE
 #endif
 
+#ifdef UNUSED
+#elif defined(__GNUC__)
+#define UNUSED(x) (void)x
+#elif defined(__LCLINT__)
+#define UNUSED(x) /*@unused@*/ x
+#elif defined(__cplusplus)
+#define UNUSED(x)
+#else
+#define UNUSED(x) x
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+
+static volatile struct LoggerConfiguration {
+    bool showFilename = false;
+    FILE* forceOutput = nullptr;
+} s_loggerConfiguration;
+
+template <typename... T>
+static inline void
+LogFormatter(FILE* output, const char* filename, uint32_t line, const char* level, const char* format, T&&... args)
+{
+    output = s_loggerConfiguration.forceOutput != nullptr ? s_loggerConfiguration.forceOutput : output;
+    if (s_loggerConfiguration.showFilename) {
+        fprintf(output, "%s:%d: ", filename, line);
+    }
+    fprintf(output, "[%s]: ", level);
+    fprintf(output, format, std::forward<T>(args)...);
+    fprintf(output, "\n");
+}
+
+#define LOG_ERROR(str, ...) LogFormatter(stderr, __FILE__, __LINE__, "ERROR", str, ##__VA_ARGS__)
+#define LOG_WARN(str, ...)  LogFormatter(stderr, __FILE__, __LINE__, "WARN", str, ##__VA_ARGS__)
+#define LOG_INFO(str, ...)  LogFormatter(stdout, __FILE__, __LINE__, "INFO", str, ##__VA_ARGS__)
+#if USING(IS_DEBUG)
+#define LOG_DEBUG(str, ...) LogFormatter(stdout, __FILE__, __LINE__, "DEBUG", str, ##__VA_ARGS__)
+#else // #if USING(IS_DEBUG)
+#define LOG_DEBUG(...)
+#endif // #else // #if USING(IS_DEBUG)
