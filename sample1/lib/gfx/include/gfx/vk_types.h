ec62a7ee65964cdaf7a63bf84bc211567940207c
marcos <markisbcn@gmail.com>
2023-09-23 18:19:35 +0200

    improved VK helpers

--- sample1/lib/gfx/include/gfx/vk_types.h
+++ sample1/lib/gfx/include/gfx/vk_types.h
@@ -5,23 +5,40 @@
 
 #include <iostream>
 
-#define VK_CHECK(x)                                              \
-    do {                                                         \
-        VkResult err = x;                                        \
-        if (err) {                                               \
-            ASSERT_MSG(false, "Detected Vulkan error: %d", err); \
-            std::abort();                                        \
-        }                                                        \
+#define VK_CHECK(x)                                       \
+    do {                                                  \
+        VkResult err = x;                                 \
+        if (err) {                                        \
+            FAIL_MSG("Vulkan error: [%d] @ %s", err, #x); \
+        }                                                 \
     } while (0)
 
-#define VK_CHECK_MSG(x, msg, ...)                                \
-    do {                                                         \
-        VkResult err = x;                                        \
-        if (err) {                                               \
-            ASSERT_MSG(false, "Detected Vulkan error: %d", err); \
-            LOG_ERROR(msg, ##__VA_ARGS__);                       \
-            std::abort();                                        \
-        }                                                        \
+#define VK_CHECK_MSG(x, msg, ...)                         \
+    do {                                                  \
+        VkResult err = x;                                 \
+        if (err) {                                        \
+            FAIL_MSG("Vulkan error: [%d] @ %s", err, #x); \
+            LOG_ERROR(msg, ##__VA_ARGS__);                \
+        }                                                 \
     } while (0)
 
 /////////////////////////////////////////////////////////////////////////////////
+#define VK_DESTROY(func, object, ...) \
+    do {                              \
+        func(object, ##__VA_ARGS__);  \
+        object = VK_NULL_HANDLE;      \
+    } while (0)
+
+#define VK_DESTROY_WITH_DEVICE(func, device, object, ...) \
+    do {                                                  \
+        func(device, object, ##__VA_ARGS__);              \
+        object = VK_NULL_HANDLE;                          \
+    } while (0)
+
+#define VK_DESTROY_LIST_WITH_DEVICE(func, device, object_list, ...)       \
+    do {                                                                  \
+        for (auto object : object_list) {                                 \
+            VK_DESTROY_WITH_DEVICE(func, _device, object, ##__VA_ARGS__); \
+        }                                                                 \
+        object_list.clear();                                              \
+    } while (0)
