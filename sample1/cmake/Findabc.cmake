420decba66929dba21f144e8ee7654f925b7a9d2
marcos <markisbcn@gmail.com>
2023-10-11 23:25:20 +0200

    added abc dependency

---
+++ sample1/cmake/Findabc.cmake
@@ -0,0 +1,24 @@
+if(TARGET abc::abc)
+    return()
+endif()
+
+message(STATUS "Third-party: creating target 'abc::abc'")
+
+include(FetchContent)
+
+message("Fetching ABC library")
+
+# if (FMT_USE_THIRDPARTY)
+#     FetchContent_Declare(
+#         fmt
+#         SOURCE_DIR "${CMAKE_SOURCE_DIR}/thirdparty/fmt/"
+#     )
+# else()
+    FetchContent_Declare(
+        abc
+        GIT_REPOSITORY https://github.com/mabaro/abc.git
+        GIT_TAG main#0.5-alpha
+    )
+# endif()
+
+FetchContent_MakeAvailable(abc)
